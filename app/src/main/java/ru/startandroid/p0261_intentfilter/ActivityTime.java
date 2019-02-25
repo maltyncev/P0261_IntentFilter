8b15a9eb85abcb3b0d9b6dab8d81d10251ad9158
maltyncev <maltyncev@gmail.com>
2019-02-25 11:50:51 +0500

    25.02 11:50

--- app/src/main/java/ru/startandroid/p0261_intentfilter/ActivityTime.java
+++ app/src/main/java/ru/startandroid/p0261_intentfilter/ActivityTime.java
@@ -1,13 +1,23 @@
 package ru.startandroid.p0261_intentfilter;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 
-public class ActivityTime extends AppCompatActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ActivityTime extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        String time = sdf.format(new Date(System.currentTimeMillis()));
+
+        TextView tvTime = (TextView) findViewById(R.id.tvTime);
+        tvTime.setText(time);
     }
 }
